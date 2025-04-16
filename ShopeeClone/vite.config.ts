17c10558667ff4e8dc20d2b5df53d15b5cc02763
bimo1102 <levanlam270920@gmail.com>
2025-04-16 21:25:00 +0700

    update code

--- ShopeeClone/vite.config.ts
+++ ShopeeClone/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -9,5 +10,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   css: {
     devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
   }
 })
